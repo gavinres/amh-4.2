7c044131db47cfb38c197d52245c1da069eb3e0d
sujianchao <jcsu@vip.qq.com>
2015-11-22 02:31:57 +0800

    Update amh-4.2.sh

--- amh-4.2.sh
+++ amh-4.2.sh
@@ -20,7 +20,7 @@ RamSwap='';
 #设置静默安装模式
 InstallModel='1';
 #Domain=`ifconfig  | grep 'inet addr:'| egrep -v ":192.168|:172.1[6-9].|:172.2[0-9].|:172.3[0-2].|:10.|:127." | cut -d: -f2 | awk '{ print $1}'`;
-Domain=`127.0.0.1`;
+Domain='127.0.0.1';
 #设置默认mysql密码
 MysqlPass='admin';
 #设置默认amh后台密码
